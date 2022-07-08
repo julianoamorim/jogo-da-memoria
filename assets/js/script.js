0c680d2bd25dbfb611d2755714837f09eada581b
Juliano Amorim <julianoamorim89@outlook.com>
2022-07-07 21:34:26 -0300

    Travando o tabuleiro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,51 @@
 const cards = document.querySelectorAll('.card');
+let cartaVirada = false;
+let primeiraCarta, segundaCarta;
+let trancarTabuleiro = false;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(trancarTabuleiro) return; //evita virar mais de duas cartas
+    if(this == primeiraCarta) return; //evita q a mesma carta seja selecionada duas vezes
+    this.classList.add('flip');
+    if(!cartaVirada){
+        cartaVirada = true;
+        primeiraCarta = this;
+        return;
+    }
+    segundaCarta = this;
+    cartaVirada = false;
+    checkFormat();
+}
+
+function checkFormat(){
+    if(primeiraCarta.dataset.card == segundaCarta.dataset.card){
+        disableCard();
+        return;
+    }
+    desvirarCarta();
+}
+
+function disableCard(){
+    primeiraCarta.removeEventListener('click', flipCard);
+    segundaCarta.removeEventListener('click', flipCard);
+}
+
+function desvirarCarta(){
+    trancarTabuleiro = true;
+    setTimeout(() => {
+        primeiraCarta.classList.remove('flip');
+        segundaCarta.classList.remove('flip');
+
+        trancarTabuleiro = false;
+    }, 1500)
+}
+
+function resetarTabuleiro(){
+    [cartaVirada, trancarTabuleiro] = [false, false];
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 });
+
+
